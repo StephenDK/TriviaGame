bcc4df2d9db47f2bebac066a8e4f49af9255dcca
StephenDK <stephendklein08@gmail.com>
2017-11-25 20:18:26 -0800

    Game is not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,31 +9,194 @@ $(document).ready(function(){
 
 	function decrement() {
 		setTimerMax--
-		$('.timer').html('<h3>' + setTimerMax + '</h2>')
+		$('.timer').html('<h3>Time Remaining: ' + setTimerMax + ' Seconds</h2>')
 	}
 
 	function stop() {
 		clearInterval(intervalID);
 	}
 
-	run();
+	
+	//These are the global variable scoreboard counters
+	var correct = 0;
+	var wrong = 0;
+
+
+// 	var questions = [{
+// 		question: "Who is the fastest of theses video game characters?",
+// 		answers: ['A. Mario',
+//  				'B. Sonic',
+//  				'C. Donkey Kong',
+//  				'D. The paperboy'],
+//  		correctAnswer: 'A. Mario'
+// 	}, {
+// 		question: "In Halo combat evolved the flood was introduced at what level?",
+// 		answers: [' A. The Pillar of Autumn',
+//  				' B. The Maw',
+//  				' C. The Library',
+//  				' D. 343 Guilty Spark'],
+//  		correctAnswer: 'C. The Library'
+//  	}, {
+//  		question: "Which of these bands was not featured in Guitar Hero III: Legends of Rock?",
+//  		answers: [' A. Iron Maiden',
+//  				' B. Weezer',
+//  				' C. Metallica',
+//  				' D. Lynard Skynyrd'],
+//  		correctAnswer: 'A. Iron Maiden'
+//  	}, {
+//  		question: "Which of the following video games takes place in a dystopian underwater city called Rapture?",
+//  		answers: [' A. Half-Life',
+// 				' B. God Of War',
+//  				' C. Fallout 3',
+//  				' D. Bioshock'],
+//  		correctAnswer: 'D. Bioshock'
+//  	}];
 	
 
-var questionsAndAnswers = {
-	q1: "Who is the fastest of theses video game characters?",
-	answer1: "",
 
-	// Mario , [Sonic], Donkey Kong, The Paperby
+// console.log(questions.question);
 
-	q2: "In Halo combat evolved the flood was introduced in what level?",
 
-	// The pillar of Autumn, [the library], The Maw, 343 Guilty Spark
 
-	q3: "Which of these bands was not featured in Guitar Hero III: Legends of Rock?"
+
+
+var question1 = {
+	question: "Who is the fastest of theses video game characters?",
+	possibleAnswers: ['A. Mario',
+				'B. Sonic',
+				'C. Donkey Kong',
+				'D. The paperboy'],
+	answer: 'B. Sonic'
+};
+
+var question2 = {
+	question: "In Halo combat evolved the flood was introduced at what level?",
+	possibleAnswers: [' A. The Pillar of Autumn',
+				' B. The Maw',
+				' C. The Library',
+				' D. 343 Guilty Spark'],
+	answer: ' C. The Library'
+};
+
+var question3 = {
+	question: "Which of these bands was not featured in Guitar Hero III: Legends of Rock?",
+	possibleAnswers: [' A. Iron Maiden',
+				' B. Weezer',
+				' C. Metallica',
+				' D. Lynard Skynyrd'],
+	answer: ' A. Iron Maiden'
+};
+
+var question4 = {
+	question: "Which of the following video games takes place in a dystopian underwater city called Rapture?",
+	possibleAnswers: [' A. Half-Life',
+				' B. God Of War',
+				' C. Fallout 3',
+				' D. Bioshock'],
+	answer: 'D. Bioshock'
 }
-	// Metallica, Weezer, [Iron Maiden], Lynard Skynyrd
+
+// // This function will add all of the questions to the html page.
+
+function loadQuestions() {
+	$('.question1').text(question1.question);
+	$('.question2').text(question2.question);
+	$('.question3').text(question3.question);
+	$('.question4').text(question4.question);
+}
 
 
+function loadAnswers() {
+	for (var i = 0; i < question1.possibleAnswers.length; i++) {
+   $('.answersQ1').append(
+   		"<input type='radio' name='question1' value=" 
+   		+ question1.possibleAnswers[i] 
+   		+ ">" 
+   		+ question1.possibleAnswers[i]
+   	);
+};
+}
+function loadAnswers2() {
+	for (var i = 0; i < question2.possibleAnswers.length; i++) {
+   $('.answersQ2').append(
+   		"<input type='radio' name='question1' value=" 
+   		+ question2.possibleAnswers[i] 
+   		+ ">" 
+   		+ question2.possibleAnswers[i]
+   	);
+};
+}
+function loadAnswers3() {
+	for (var i = 0; i < question3.possibleAnswers.length; i++) {
+   $('.answersQ3').append(
+   		"<input type='radio' name='question1' value=" 
+   		+ question3.possibleAnswers[i] 
+   		+ ">" 
+   		+ question3.possibleAnswers[i]
+   	);
+};
+}
+function loadAnswers4() {
+	for (var i = 0; i < question4.possibleAnswers.length; i++) {
+   $('.answersQ4').append(
+   		"<input type='radio' name='question1' value=" 
+   		+ question4.possibleAnswers[i] 
+   		+ ">" 
+   		+ question4.possibleAnswers[i]
+   	);
+};
+}
+
+function gameFinishButton(){
+	$("#submit").append("<button class='submitTestForResults'>Submit</button");
+}
+
+
+function answerChecker() {
+   $.each($("input=[name='question1']:checked"), function() {
+      if ($(this).val() === questions.correctAnswer) {
+        correct++;
+      } else {
+       wrong++;
+      }
+   });
+}
+
+
+
+
+function scoarBoard() {
+	$('#scoarBoard').append('<h2>Number of questions correct' + correct + '</h2>')
+}	$('#scoarBoard').append('<h2>Number of questions correct' + wrong + '</h2>')
+
+	//When the start button is clicked hide the start button and run clock
+	//Also run the loadQuestions and loadAnswers functions.
+
+	$('#startButton').on('click', function(){
+		$(this).hide();
+		loadQuestions();
+		loadAnswers();
+		loadAnswers2();
+		loadAnswers3();
+		loadAnswers4();
+		run();
+		gameFinishButton();
+	});
+
+		
+		$('submitTestForResults').on('click', function(){
+			answerChecker();
+			scoreboard();
+		})
+
+		if (intervalID === 0) {
+			answerChecker();
+			scoreboard();
+		}
+
+		
+	
+	
 });
 
 
@@ -51,36 +214,5 @@ var questionsAndAnswers = {
 
 
 
-// $(document).ready(function() {
-// 	var index = 0;					//set global index variable to 0
-// 	var countdownTimer = {			// set JSON var tto countdown timer
-// 		time : 30,					// set the max time to 30 seconds
-// 		reset: function() {				// Create a reset timer function 
-// 			this.time = 30;					//reset countdown timer to 30 seconds
-// 			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>'); //update html
-// 		},
-// 		start: function() {						//start timer
-// 			counter = setInterval(countdownTimer.count, 1000);	
-// 		},
-// 		stop: function() {
-// 			clearInterval(counter);
-// 		},
-// 		count: function() {
-// 				countdownTimer.time--;
-// 				console.log(countdownTimer.time);
-// 			if (countdownTimer.time >= 0) {
-// 				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
-// 			}
-// 			else {
-// 				index++;
-// 				answerWrong();
-// 				countdownTimer.reset();
-// 				if (index < questionArray.length) {
-// 					loadQuestion(index);
-// 				} else {
-// 					$(".answerchoice").hide();
-// 					showScore();
-// 				}
-// 			}
-// 		}
-// 	}
+
+
